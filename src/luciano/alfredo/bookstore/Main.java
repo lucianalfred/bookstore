2ffd2013148a3ffbfe2f9fc874afd2063533058e
Luciano Alfredo <lucianalfred60@gmail.com>
2026-03-19 15:46:07 +0000

    List book

--- src/luciano/alfredo/bookstore/Main.java
+++ src/luciano/alfredo/bookstore/Main.java
@@ -1,8 +1,21 @@
 package luciano.alfredo.bookstore;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import luciano.alfredo.bookstore.data.Books;
+import luciano.alfredo.bookstore.model.Book;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("\nLista de Livros disponiveis \n");
+		
+		List <Book> books = Books.all();
+		
+		IntStream.range(0, books.size())
+			.forEach(i -> {
+				System.out.println(i + " - " + books.get(i).getName());
+			});
 	}
 }
